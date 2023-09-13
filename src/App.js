816ef71ef8f050bf0403eb76a28e091a3e2d721f
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2023-09-13 14:05:40 +0200

    msw intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const jsonResponse = await response.json();
+      setAdvice(jsonResponse.slip);
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p className="inspo-quote">
+        {advice ? `"${advice.advice}"` : "Loading.."}
+      </p>
+      <p className="girly-things">Just girly things</p>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// standardimport från msw
+import { render, screen, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+//används för att sätta upp servern
+import { setupServer } from "msw/node";
+import App from "./App";
 
-test('renders learn react link', () => {
+// sätter upp msw att lyssna på nedanstående URL, alla requests till den URL:en kontrollerar vi.
+// Vi returnerar vårt egna objekt med rådet YOLO
+const server = setupServer(
+  rest.get("https://api.adviceslip.com/advice", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        slip: { id: 107, advice: "YOLO" },
+      })
+    );
+  })
+);
+
+//INNAN alla tester säger vi åt servern att lyssna
+//beforeAll() är en hook som ingår i jest
+beforeAll(() => server.listen());
+
+//EFTER alla tester säger vi åt servern att stänga ner sig
+afterAll(() => server.close());
+
+test("should generate a quote", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //Väntar på att loading försvinner
+  await waitFor(() =>
+    expect(screen.queryByText("Loading..")).not.toBeInTheDocument()
+  );
+  //Kollar att YOLO faktiskt finns i dokumentet
+  expect(screen.getByText('"YOLO"')).toBeInTheDocument();
 });
